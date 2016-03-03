ec7b0f0876d4550e7446c117b7dcb0c337f87e6a
Gabrielle M Gussin <gmg302@login-0-3.local>
2016-03-03 12:31:13 -0500

    antibiotic-res-BLAST.sh

---
+++ antibiotic-res-BLAST.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#PBS -S /bin/bash
+#PBS -N AR-blast
+#PBS -l nodes=1:ppn=12,walltime=24:00:00,mem=24GB
+#PBS -q s48
+#PBS -M gmg302@nyu.edu
+#PBS -m ea
+#PBS -e localhost:${PBS_O_WORKDIR}/${PBS_JOBNAME}.e${PBS_JOBID}
+#PBS -o localhost:${PBS_O_WORKDIR}/${PBS_JOBNAME}.o${PBS_JOBID}
+
+file=$1
+
+dir="/scratch/at120/shared/gabby-alan/2015-10-01_C8364ACXX-redo/antbiotic-resistance"
+path="/scratch/at120/shared/gabby-alan/2015-10-01_C8364ACXX-redo/filtered-data"
+
+cd $dir
+
+module load blast+
+
+#1. download the antibiotic resistance database (nucleotide fasta) from the ARG-ANNOT website
+#ARGANNOT NT_V2_15Dec_2015:
+#wget http://www.mediterranee-infection.com/arkotheque/client/ihumed/_depot_arko/articles/691/argannot-nt_doc.fasta
+
+#2. create custom BLAST database for the antibiotic resistance genes using makeblastdb blast+ package
+#makeblastdb -in file -out name -dbtype prot/nucl -hash_index for a database of DNA or RNA
+#makeblastdb -in argannot-nt_doc.fasta -out antibiotic-resistance -dbtype nucl -hash_index -parse_seqids
+
+#3. blastn to custom databas
+blastn -task blastn -query $path/$file.non-rRNA.deconseq_clean.mask.fasta -out $dir/$file.ARgene.blast.tsv -outfmt 6 -evalue 0.00001 -max_target_seqs 1 -culling_limit 2 -num_threads 12 -db $dir/antibiotic-resistance
+   
+exit 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
